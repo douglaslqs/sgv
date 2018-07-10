494d83360144232a01e4f94b2c9f3a7376080675
Douglas Santos <douglasrock15@hotmail.com>
2018-07-10 12:49:43 -0300

    #53 - Adicionado layout a estrutura

--- module/Administrator/config/module.config.php
+++ module/Administrator/config/module.config.php
@@ -58,5 +58,16 @@ return [
         'template_path_stack' => [
             'Administrator' => __DIR__ . '/../view',
         ],
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => [
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'administrator/index/index' => __DIR__ . '/../view/administrator/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ],
     ],
 ];
